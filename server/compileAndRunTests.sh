81b2ad5c350866d20e3c6284da9cffdbdead9f0e
U-DESKTOP-CIBLUH6\marie <marie.j.persson@gmail.com>
2018-12-21 12:34:04 +0100

    removed unused file

--- server/compileAndRunTests.sh
+++
@@ -1,4 +0,0 @@
-#Testing the Restaurant class
-# Compile and run
-javac -cp ".;webroot/WEB-INF/lib/org.json.jar;./winstone.jar;webroot/WEB-INF/classes" webroot/WEB-INF/classes/test/TestRestaurant.java
-java -cp ".;webroot/WEB-INF/classes" test.TestRestaurant
